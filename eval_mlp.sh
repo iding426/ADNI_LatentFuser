90e46bfa96553c4cbf3398648d0c959524920869
Ian Ding <ianding@crc-dot1x-nat-10-239-23-68.bu.edu>
2025-11-10 21:28:20 -0500

    Fixing eval bashes

---
+++ eval_mlp.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#$ -P cs599dg      # Specify the SCC project name you want to use
+#$ -l h_rt=48:00:00   # Specify the hard time limit for the job
+#$ -l mem_per_core=16G
+#$ -l gpus=1
+
+source ~/.bashrc
+mkdir -p logs
+
+module load miniconda
+conda activate fusion_model
+
+python scripts/eval.py --fusion-model mlp --vae-ckpt checkpoint/medvae3d_076000.pt \
+    --fusion-ckpt weights/fusion_mlp/mlp_fusion_final.pt \
+    --output-dir eval_results/fusion_mlp \
+    --test-csv data/triplets_eval.csv
---
+++ eval_transformer.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#$ -P cs599dg      # Specify the SCC project name you want to use
+#$ -l h_rt=48:00:00   # Specify the hard time limit for the job
+#$ -l mem_per_core=16G
+#$ -l gpus=1
+
+source ~/.bashrc
+mkdir -p logs
+
+module load miniconda
+conda activate fusion_model
+
+python scripts/eval.py --fusion-model transformer --vae-ckpt checkpoint/medvae3d_076000.pt \
+    --fusion-ckpt weights/fusion_transformer/transformer_fusion_final.pt \
+    --output-dir eval_results/fusion_transformer \
+    --test-csv data/triplets_eval.csv
